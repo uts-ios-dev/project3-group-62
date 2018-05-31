d4a159a90c90fc5dc6f2d85a7132ef8442fcd1bf
Peter Bower <doctor_bower@hotmail.com>
2018-05-31 22:12:34 +1000

    Checkpoint - Summation Halfway Mark
    Added Summation View Controller
    Is not quite working yet but is most of the way there. Need to get the '+' subscript signs to appear in the right spot, and the constraints working so the text field is the right size.
    Will still need to add a translation for it and then add a 'Product' version (which should be simple enough).
    Also made the navigation controller the initial view controller.

---
+++ Math Notation To Language Converter/Math Notation To Language Converter/Controller/SummationProductVC.swift
@@ -0,0 +1,89 @@
+//
+//  SummationProductVC.swift
+//  Math Notation To Language Converter
+//
+//  Created by Peter Bower on 31/5/18.
+//  Copyright © 2018 Summer Studios. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SummationProductVC: UIViewController {
+    
+    var summation: MathSequence
+    
+    required init?(coder aDecoder: NSCoder) {
+        summation = MathSequence(sequenceVariable: "i", startValue: "1", endValue: "100", sequence: "a")
+        super.init(coder: aDecoder)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        sequenceVariableTf.text = summation.sequenceVariable
+        startValueTf.text = summation.startValue
+        endValueTf.text = summation.endValue
+        formulaTf.text = summation.updateFormula(sequenceType: .summation)
+        sequenceTf.text = summation.sequence
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBOutlet weak var sequenceVariableTf: UITextField!
+    
+    @IBOutlet weak var startValueTf: UITextField!
+    
+    @IBOutlet weak var endValueTf: UITextField!
+    
+    @IBOutlet weak var sequenceTf: UITextField!
+    
+    @IBAction func sequenceVariableUpdated(_ sender: UITextField) {
+        if let newText = sequenceVariableTf.text {
+            summation.sequenceVariable = newText
+        } else {
+            summation.sequenceVariable = "i"
+        }
+        startValueSummation.text = "\(sequenceVariableTf.text ?? "i")=\(startValueTf.text ?? "1")"
+        formulaTf.text = summation.updateFormula(sequenceType: .summation)
+    }
+    
+    @IBAction func startValueUpdated(_ sender: UITextField) {
+        if let newText = startValueTf.text {
+            summation.startValue = newText
+        } else {
+            summation.startValue = "i"
+        }
+        startValueSummation.text = "\(sequenceVariableTf.text ?? "i")=\(startValueTf.text ?? "1")"
+        formulaTf.text = summation.updateFormula(sequenceType: .summation)
+    }
+    
+    
+    @IBAction func endValueUpdated(_ sender: UITextField) {
+        if let newText = endValueTf.text {
+            summation.endValue = newText
+        } else {
+            summation.endValue = "100"
+        }
+        endValueSummation.text = endValueTf.text
+        formulaTf.text = summation.updateFormula(sequenceType: .summation)
+    }
+    
+    @IBAction func sequenceUpdated(_ sender: UITextField) {
+        if let newText = sequenceTf.text {
+            summation.sequence = newText
+        } else {
+            summation.sequence = "a"
+        }
+        formulaTf.text = summation.updateFormula(sequenceType: .summation)
+    }
+    
+    @IBOutlet weak var formulaTf: UITextField!
+    
+    @IBOutlet weak var startValueSummation: UITextField!
+    @IBOutlet weak var endValueSummation: UITextField!
+
+    
+}
