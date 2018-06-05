ca92fb4400a09aff6318f3d2b5193f0362def02d
ヌルポ <episodes27@gmail.com>
2018-06-05 23:42:05 +1000

    fixed firebase lanuch issue

--- Math Notation To Language Converter/Math Notation To Language Converter/Controller/SearchVC.swift
+++ Math Notation To Language Converter/Math Notation To Language Converter/Controller/SearchVC.swift
@@ -5,7 +5,6 @@
 //  Created by サイラク on 2018/5/29.
 //  Copyright © 2018年 Summer Studios. All rights reserved.
 //
-
 import UIKit
 class SearchTableViewCell: UITableViewCell {
     @IBOutlet weak var labelLeft: UILabel!
@@ -19,8 +18,8 @@ class SearchViewController: UIViewController,UITableViewDelegate,UITableViewData
     @IBOutlet weak var searchBar: UISearchBar!
     
     //Data
-    let dictionary = Dictionary()
-   
+    var dictionary : Dictionary?
+    
     var filterData = [Symbol]()
     var isSeaching = false
     
@@ -28,9 +27,11 @@ class SearchViewController: UIViewController,UITableViewDelegate,UITableViewData
         if isSeaching {
             return filterData.count
         }
-        return dictionary.symbolArray.count
+        if let dic = dictionary {
+            return dic.symbolArray.count
+        }
+        return 0
     }
-    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         var symbolDetailVC: SymbolDetailVC
         symbolDetailVC = self.storyboard?.instantiateViewController(withIdentifier: "symbolDetail") as! SymbolDetailVC
@@ -38,23 +39,29 @@ class SearchViewController: UIViewController,UITableViewDelegate,UITableViewData
         if isSeaching {
             symbolDetailVC.symbol = filterData[indexPath.row]
         } else {
-            symbolDetailVC.symbol = dictionary.symbolArray[indexPath.row]
+            if let dic = dictionary {
+                symbolDetailVC.symbol = dic.symbolArray[indexPath.row]
+            }
+            
         }
         navigationController?.pushViewController(symbolDetailVC,animated:true)
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! SearchTableViewCell
-        if isSeaching {
-            cell.labelLeft.text = filterData[indexPath.row].symbol
-            cell.labelRight.text = filterData[indexPath.row].name.trunc(length: 15)
-            cell.symbol = filterData[indexPath.row]
-        } else {
-            cell.labelLeft.text = dictionary.symbolArray[indexPath.row].symbol
-            cell.labelRight.text = dictionary.symbolArray[indexPath.row].name.trunc(length: 15)
-            cell.symbol = dictionary.symbolArray[indexPath.row]
-            
+        if let dic = dictionary {
+            if isSeaching {
+                cell.labelLeft.text = filterData[indexPath.row].symbol
+                cell.labelRight.text = filterData[indexPath.row].name.trunc(length: 15)
+                cell.symbol = filterData[indexPath.row]
+            } else {
+                cell.labelLeft.text = dic.symbolArray[indexPath.row].symbol
+                cell.labelRight.text = dic.symbolArray[indexPath.row].name.trunc(length: 15)
+                cell.symbol = dic.symbolArray[indexPath.row]
+                
+            }
         }
+        
         return cell
     }
     
@@ -65,11 +72,14 @@ class SearchViewController: UIViewController,UITableViewDelegate,UITableViewData
             tableView.reloadData()
         } else {
             isSeaching = true
-            filterData = dictionary.symbolArray.filter({$0.symbol.lowercased().range(of:searchBar.text!.lowercased()) != nil || $0.name.lowercased().range(of:searchBar.text!.lowercased()) != nil})
+            if let dic = dictionary {
+                filterData = dic.symbolArray.filter({$0.symbol.lowercased().range(of:searchBar.text!.lowercased()) != nil || $0.name.lowercased().range(of:searchBar.text!.lowercased()) != nil})
+            }
+            
             tableView.reloadData()
         }
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
@@ -77,15 +87,17 @@ class SearchViewController: UIViewController,UITableViewDelegate,UITableViewData
         searchBar.delegate = self
         searchBar.returnKeyType = UIReturnKeyType.done
         
-      
+        //Init dictionary
+        dictionary = Dictionary()
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-  
-   
-   
-
+    
+    
+    
+    
 }
