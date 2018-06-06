8d631c008f1584a589f7269b4d11930e3a7d748d
Peter Bower <doctor_bower@hotmail.com>
2018-06-06 21:51:46 +1000

    Added Buttons to Formula Selection Page + Changed Some UI Titles for Clarity
    Added Buttons to Formula Selection Page
    Changed Some UI Titles for Clarity

--- Math Notation To Language Converter/Math Notation To Language Converter/Model/Main.swift
+++
@@ -1,9 +0,0 @@
-//
-//  main.swift
-//  Math Notation To Language Converter
-//
-//  Created by Hitachi on 平成30-05-24.
-//  Copyright © 平成30年 Summer Studios. All rights reserved.
-//
-
-import Foundation
